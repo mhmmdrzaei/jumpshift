cdafc4f147ea4efb1bbc16b6af252d16e36acbc6
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2017-12-21 16:32:53 -0500

    blerg today

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 	<?php // Load Meta ?>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">
+
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <!-- stylesheets should be enqueued in functions.php -->
